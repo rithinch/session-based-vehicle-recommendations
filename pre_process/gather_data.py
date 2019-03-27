f6519463798da4a899e0e8d83020139026c3b269
Rithin Chalumuri <chalumuv@uni.coventry.ac.uk>
2019-03-27 18:30:42 +0000

    Added Query String filter

--- pre_process/gather_data.py
+++ pre_process/gather_data.py
@@ -5,7 +5,9 @@ extracted_values = {}
 errors = []
 
 def get_details(page_url):
-    
+
+    page_url = page_url.split("?")[0]
+
     if page_url in extracted_values:
         return extracted_values[page_url]
     
@@ -38,27 +40,32 @@ def get_details(page_url):
 
     return details
 
-files_path = "sample_data/*.csv"
+def concat_csv_files(files_path):
 
-filenames = glob.glob(files_path)
+    filenames = glob.glob(files_path, recursive=True)
 
-combined_csv = pd.concat([pd.read_csv(f, skiprows=6) for f in filenames])
+    combined_csv = pd.concat([pd.read_csv(f, skiprows=6) for f in filenames])
 
-combined_csv['Reg No'] = combined_csv['Page'].apply(lambda x: get_details(x)['reg_no'])
-combined_csv['Make'] = combined_csv['Page'].apply(lambda x: get_details(x)['make'])
-combined_csv['Model'] = combined_csv['Page'].apply(lambda x: get_details(x)['model'])
-combined_csv['Fuel'] = combined_csv['Page'].apply(lambda x: get_details(x)['fuel'])
-combined_csv['Colour'] = combined_csv['Page'].apply(lambda x: get_details(x)['colour'])
-combined_csv['Body'] = combined_csv['Page'].apply(lambda x: get_details(x)['body'])
-combined_csv['Trasmission'] = combined_csv['Page'].apply(lambda x: get_details(x)['transmission'])
+    combined_csv['Reg No'] = combined_csv['Page'].apply(lambda x: get_details(x)['reg_no'])
+    combined_csv['Make'] = combined_csv['Page'].apply(lambda x: get_details(x)['make'])
+    combined_csv['Model'] = combined_csv['Page'].apply(lambda x: get_details(x)['model'])
+    combined_csv['Fuel'] = combined_csv['Page'].apply(lambda x: get_details(x)['fuel'])
+    combined_csv['Colour'] = combined_csv['Page'].apply(lambda x: get_details(x)['colour'])
+    combined_csv['Body'] = combined_csv['Page'].apply(lambda x: get_details(x)['body'])
+    combined_csv['Trasmission'] = combined_csv['Page'].apply(lambda x: get_details(x)['transmission'])
 
-combined_csv.to_csv("vehicle_page_views.csv", index=False)
+    combined_csv.to_csv("vehicle_page_views.csv", index=False)
 
-print(combined_csv.head())
+    print(combined_csv.head())
 
-error_data = pd.DataFrame(data={"Page URL": errors})
-print(f"Skipped URL's (Without Reg No): {len(errors)}")
-error_data.to_csv("invalid_page_urls.csv", index=False)
+    error_data = pd.DataFrame(data={"Page URL": errors})
+    print(f"Skipped URL's (Without Reg No): {len(errors)}")
+    error_data.to_csv("invalid_page_urls.csv", index=False)
+
+files_path = "real_data/**/*.csv"
+
+concat_csv_files(files_path)
+
 
 
     
