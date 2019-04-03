4d8ab4061bbbb6e2a275b26be07404064f5a06f6
Rithin Chalumuri <chalumuv@uni.coventry.ac.uk>
2019-04-04 00:54:08 +0100

    Updated score.py to use model class instead of declaring it new

--- model/score.py
+++ model/score.py
@@ -1,109 +1,8 @@
-##############################################################################
-# MODEL CODE - DON'T REMOVE
-##############################################################################
-
-import math
-import numpy as np
-import torch
-from torch import nn
-from torch.nn import Module, Parameter
-import torch.nn.functional as F
-
-class GNN(Module):
-    def __init__(self, hidden_size, step=1):
-        super(GNN, self).__init__()
-        self.step = step
-        self.hidden_size = hidden_size
-        self.input_size = hidden_size * 2
-        self.gate_size = 3 * hidden_size
-        self.w_ih = Parameter(torch.Tensor(self.gate_size, self.input_size))
-        self.w_hh = Parameter(torch.Tensor(self.gate_size, self.hidden_size))
-        self.b_ih = Parameter(torch.Tensor(self.gate_size))
-        self.b_hh = Parameter(torch.Tensor(self.gate_size))
-        self.b_iah = Parameter(torch.Tensor(self.hidden_size))
-        self.b_oah = Parameter(torch.Tensor(self.hidden_size))
-
-        self.linear_edge_in = nn.Linear(self.hidden_size, self.hidden_size, bias=True)
-        self.linear_edge_out = nn.Linear(self.hidden_size, self.hidden_size, bias=True)
-        self.linear_edge_f = nn.Linear(self.hidden_size, self.hidden_size, bias=True)
-
-    def GNNCell(self, A, hidden):
-        input_in = torch.matmul(A[:, :, :A.shape[1]], self.linear_edge_in(hidden)) + self.b_iah
-        input_out = torch.matmul(A[:, :, A.shape[1]: 2 * A.shape[1]], self.linear_edge_out(hidden)) + self.b_oah
-        inputs = torch.cat([input_in, input_out], 2)
-        gi = F.linear(inputs, self.w_ih, self.b_ih)
-        gh = F.linear(hidden, self.w_hh, self.b_hh)
-        i_r, i_i, i_n = gi.chunk(3, 2)
-        h_r, h_i, h_n = gh.chunk(3, 2)
-        resetgate = torch.sigmoid(i_r + h_r)
-        inputgate = torch.sigmoid(i_i + h_i)
-        newgate = torch.tanh(i_n + resetgate * h_n)
-        hy = newgate + inputgate * (hidden - newgate)
-        return hy
-
-    def forward(self, A, hidden):
-        for i in range(self.step):
-            hidden = self.GNNCell(A, hidden)
-        return hidden
-
-class SessionGraph(Module):
-    def __init__(self, opt, n_node):
-        super(SessionGraph, self).__init__()
-        self.hidden_size = opt.hiddenSize
-        self.n_node = n_node
-        self.batch_size = opt.batchSize
-        self.nonhybrid = opt.nonhybrid
-        self.embedding = nn.Embedding(self.n_node, self.hidden_size)
-        self.gnn = GNN(self.hidden_size, step=opt.step)
-        self.linear_one = nn.Linear(self.hidden_size, self.hidden_size, bias=True)
-        self.linear_two = nn.Linear(self.hidden_size, self.hidden_size, bias=True)
-        self.linear_three = nn.Linear(self.hidden_size, 1, bias=False)
-        self.linear_transform = nn.Linear(self.hidden_size * 2, self.hidden_size, bias=True)
-        self.loss_function = nn.CrossEntropyLoss()
-        self.optimizer = torch.optim.Adam(self.parameters(), lr=opt.lr, weight_decay=opt.l2)
-        self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=opt.lr_dc_step, gamma=opt.lr_dc)
-        self.reset_parameters()
-
-    def reset_parameters(self):
-        stdv = 1.0 / math.sqrt(self.hidden_size)
-        for weight in self.parameters():
-            weight.data.uniform_(-stdv, stdv)
-
-    def compute_scores(self, hidden, mask):
-        ht = hidden[torch.arange(mask.shape[0]).long(), torch.sum(mask, 1) - 1]  # batch_size x latent_size
-        q1 = self.linear_one(ht).view(ht.shape[0], 1, ht.shape[1])  # batch_size x 1 x latent_size
-        q2 = self.linear_two(hidden)  # batch_size x seq_length x latent_size
-        alpha = self.linear_three(torch.sigmoid(q1 + q2))
-        a = torch.sum(alpha * hidden * mask.view(mask.shape[0], -1, 1).float(), 1)
-        if not self.nonhybrid:
-            a = self.linear_transform(torch.cat([a, ht], 1))
-        b = self.embedding.weight[1:]  # n_nodes x latent_size
-        scores = torch.matmul(a, b.transpose(1, 0))
-        return scores
-
-    def forward(self, inputs, A, mask, alias_inputs):
-        
-        hidden = self.embedding(inputs)
-        
-        hidden = self.gnn(A, hidden)
-        
-        hidden = torch.stack([hidden[i][alias_inputs[i]] for i in torch.arange(len(alias_inputs)).long()])
-        
-        return self.compute_scores(hidden, mask)
-
-def trans_to_cuda(variable):
-    if torch.cuda.is_available():
-        return variable.cuda()
-    else:
-        return variable
-
-##############################################################################
-
 import json
 import os
 import pickle
 import argparse
-
+from model import *
 from azureml.core.model import Model
 
 opt = argparse.Namespace()
@@ -187,7 +86,7 @@ def post_process(predictions):
 
 def run(raw_data):
     
-    data = raw_data if local else np.array(json.loads(raw_data)['data']) #List of reg_no clicks eg [1,2,3,4]
+    data = raw_data if local else np.array(json.loads(raw_data)['viewed_vehicles']) #List of reg_no clicks eg [1,2,3,4]
     
     items, A, mask, alias_inputs = pre_process(data)
 
